a66435850896ba15601af451cb2d94b30e2adb46
Chisomo Chiweza <chisomochiweza@proton.me>
2026-01-02 19:48:08 +0200

    Setup EazyTicket app: - Setup server using gin - Setup endpoint for verifying token from meta - Setup configuration file (.env)

--- app/main.go
+++ app/main.go
@@ -1,5 +1,52 @@
-package eazyticket
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	verificationToken := os.Getenv("META_VERIFY_TOKEN")
+	if verificationToken == "" {
+		panic("META_VERIFY_TOKEN not set in .env file")
+	}
+
+	router := gin.Default()
+
+	router.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{"message": "Welcome to eazy ticket!"})
+	})
+
+	router.GET("/eazyticket", func(context *gin.Context) {
+
+		mode := context.Query("hub.mode")
+		token := context.Query("hub.verify_token")
+		challenge := context.Query("hub.challenge")
+
+		if mode == "subscribe" && token == "my_verify_token" {
+			context.String(http.StatusOK, challenge)
+		} else {
+			context.JSON(http.StatusForbidden, "Verification failed")
+		}
+	})
+
+	router.POST("/eazyticket", func(context *gin.Context) {
+
+		// TODO: Read incoming webhook data and process it using flow library
+
+		context.JSON(http.StatusOK, "EVENT_RECEIVED")
+
+	})
+
+	router.Run(":9000")
 
 }
